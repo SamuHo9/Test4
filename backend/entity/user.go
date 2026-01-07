6abe5b30ae2eeefb19034803d7f527ff6bb97c6d
OS <samsion0991713667@gmail.com>
2026-01-07 10:41:19 +0700

    SweetDynami -close #1

--- backend/entity/user.go
+++ backend/entity/user.go
@@ -1,1 +1,8 @@
 package entity
+
+type User struct {
+	Name  string `valid:"required~Error Name is required"`
+	Email string `valid:"email~Error Email is invalid,required"`
+	Age   uint    `valid:"range(18|150)~Error Age must be between 18 and 150"`
+
+}
